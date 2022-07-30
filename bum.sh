83440d923e114a4c677af847b84d8056996f5cbf
Andrew Sarver <22250203+astromodder@users.noreply.github.com>
2022-07-29 20:26:51 -0400

    Add first pass at function

---
+++ bum.sh
@@ -0,0 +1,56 @@
+bum () {
+  case $1 in
+
+  help)
+    echo "Help Messages"
+    ;;
+  
+  update)
+    case $2 in
+
+    $3)
+      brew upgrade $3
+      ;;
+    
+    *)
+      brew upgrade
+      ;;
+    esac
+    ;;
+
+  list)
+    case $2 in
+
+    installed)
+      brew list
+      ;;
+    
+    *)
+      echo "unknown"
+      ;;
+    esac
+    ;;
+  
+  install)
+    # This should be better
+    brew install --cask $2
+    brew install $2
+    ;;
+  
+  reinstall)
+    # This should be better
+    brew reinstall --cask $2
+    brew reinstall $2
+    ;;
+  
+  search)
+    brew search $2
+    ;;
+
+  *)
+    echo -n "unknown"
+    ;;
+esac
+}
+
+bum $@
